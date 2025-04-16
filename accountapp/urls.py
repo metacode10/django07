119133f033a376095a694680cf6d7aa33f584969
Yoonsung Kang <metacode10@gmail.com>
2025-04-17 00:30:12 +0900

    a

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -3,7 +3,7 @@ from tempfile import template
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accountapp.views import hello_world, AccountCreateVeiw, AccountDetailView, AccountUpdateView
+from accountapp.views import hello_world, AccountCreateVeiw, AccountDetailView, AccountUpdateView, AccountDeleteView
 
 app_name = "accountapp"
 
@@ -16,4 +16,5 @@ urlpatterns = [
     path('create/', AccountCreateVeiw.as_view(), name='create'),
     path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
     path('update/<int:pk>', AccountUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>', AccountDeleteView.as_view(), name='delete'),
 ]
