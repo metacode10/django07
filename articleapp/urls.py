40a7b72a211b69d3e1c18c926c73ae27340a0014
Yoonsung Kang <metacode10@gmail.com>
2025-04-22 01:34:24 +0900

    a

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,13 +1,18 @@
 from tempfile import template
 
-from django.contrib.auth.views import LoginView, LogoutView
+
 from django.urls import path
 from django.views.generic import TemplateView
 
-from profileapp.views import ProfileCreateView, ProfileUpdateView
+from articleapp.views import AritcleCreateView, AritcleDetailView, AritcleUpdateView, AritcleDeleteView
 
 app_name = "articleapp"
 
 urlpatterns = [
     path('list/', TemplateView.as_view(template_name='articleapp/list.html'), name='list'),
+
+    path('create/', AritcleCreateView.as_view(), name='create'),
+    path('detail/<int:pk>', AritcleDetailView.as_view(), name='detail'),
+    path('update/<int:pk>', AritcleUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>', AritcleDeleteView.as_view(), name='delete'),
 ]
--- articleapp/views.py
+++ articleapp/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
+from articleapp.decorators import article_ownership_required
 from articleapp.forms import AritcleCreationForm
 from articleapp.models import Article
 
@@ -22,7 +24,7 @@ class AritcleCreateView(CreateView):
         return super().form_valid(form)
 
     def get_success_url(self):
-        return reversed('articleapp:detail', kwargs={'pk': self.object.pk})
+        return reverse('articleapp:detail', kwargs={'pk': self.object.pk})
 
 
 
@@ -30,3 +32,27 @@ class AritcleDetailView(DetailView):
     model = Article
     context_object_name = 'target_article'
     template_name = 'articleapp/detail.html'
+
+
+
+@method_decorator(article_ownership_required, 'get')
+@method_decorator(article_ownership_required, 'post')
+class AritcleUpdateView(UpdateView):
+    model = Article
+    context_object_name = 'target_article'
+    form_class = AritcleCreationForm
+    template_name = 'articleapp/update.html'
+
+
+    def get_success_url(self):
+        return reverse('articleapp:detail', kwargs={'pk': self.object.pk})
+
+
+@method_decorator(article_ownership_required, 'get')
+@method_decorator(article_ownership_required, 'post')
+class AritcleDeleteView(DeleteView):
+    model = Article
+    context_object_name = 'target_article'
+    success_url = reverse_lazy('articleapp:list')
+    template_name = 'articleapp/delete.html'
+
