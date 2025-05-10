fe96a23e2f81061e2589a7e96e2f1f668f2116b6
Yoonsung Kang <metacode10@gmail.com>
2025-05-11 01:18:37 +0900

    abc

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -4,13 +4,14 @@ from tempfile import template
 from django.urls import path
 from django.views.generic import TemplateView
 
-from articleapp.views import AritcleCreateView, AritcleDetailView, AritcleUpdateView, AritcleDeleteView
+from articleapp.views import AritcleCreateView, AritcleDetailView, AritcleUpdateView, AritcleDeleteView, AritcleListView
 
 app_name = "articleapp"
 
 urlpatterns = [
-    path('list/', TemplateView.as_view(template_name='articleapp/list.html'), name='list'),
+    # path('list/', TemplateView.as_view(template_name='articleapp/list.html'), name='list'),
 
+    path('list/', AritcleListView.as_view(), name='list'),
     path('create/', AritcleCreateView.as_view(), name='create'),
     path('detail/<int:pk>', AritcleDetailView.as_view(), name='detail'),
     path('update/<int:pk>', AritcleUpdateView.as_view(), name='update'),
--- articleapp/views.py
+++ articleapp/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
-from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView, ListView
 
 from articleapp.decorators import article_ownership_required
 from articleapp.forms import AritcleCreationForm
@@ -56,3 +56,9 @@ class AritcleDeleteView(DeleteView):
     success_url = reverse_lazy('articleapp:list')
     template_name = 'articleapp/delete.html'
 
+
+class AritcleListView(ListView):
+    model = Article
+    context_object_name = 'article_list'
+    template_name = 'articleapp/list.html'
+    paginate_by = 25
